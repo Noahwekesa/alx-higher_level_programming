7c6fef429b531d67d90c27bebe000c3ee2210076
noahwksa <noahwekesa0gmail.com>
2023-08-09 21:10:09 +0300

    king

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for first_digit in range(0, 8):
-	for second_number in (first_digit+1, 10):
-		print("{:d}, {:d}".format(first_digit, second_number), end=", ")
+	for second_number in (1, 10):
+		if first_digit == 8  and second_number == 10:
+			print("{:d}{:d}".format(first_digit, second_number), end=", ")
+	else:
+		print("{:d}{:d}".format(first_digit, second_number))
