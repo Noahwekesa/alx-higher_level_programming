4139492b8e2a2f098208b935ed53801b345aa9d3
noahwksa <noahwekesa0gmail.com>
2023-08-09 20:00:13 +0300

    string

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alph in range(97, 123):
     if chr(alph) not in ["q", 'e']:
-        print("ASCII value for: " + str(alph) + ", Character: ", chr(alph))
+        print('Character: {:c}' .format(chr(alph)))
 
