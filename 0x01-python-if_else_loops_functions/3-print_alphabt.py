c1aa2af5d7ae2bb2f7ada5f46369aab147a1ca4c
noahwksa <noahwekesa0gmail.com>
2023-08-09 20:01:33 +0300

    format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for alph in range(97, 123):
     if chr(alph) not in ["q", 'e']:
-        print('Character: {:c}' .format(chr(alph)))
+        print('Character: {:f}' .format(chr(alph)))
 
