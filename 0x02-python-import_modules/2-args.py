650e391b3eb7129c807b62ec09e8311ee7a9e4ca
noahwksa <noahwekesa0gmail.com>
2023-08-11 16:42:29 +0300

    hurt feelings

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,10 @@
 if __name__ == '__main__':
 
     import sys
-    arguments = sys.argv[1:]
-    num_arg = len(arguments)
+    num_arg = len(sys.argv) - 1
     print('{} arguments.'.format(num_arg))
     if num_arg == 0:
         print("")
     else:
-        for num_arg, arg in enumerate(arguments, start=1):
+        for num_arg, arg in enumerate(num_arg, start=1):
             print("{}: {}".format(num_arg, arg))
