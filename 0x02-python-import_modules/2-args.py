fcb95f44715bec15ee7322f88131c5167f7bcbea
noahwksa <noahwekesa0gmail.com>
2023-08-11 16:38:04 +0300

    matter

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     num_arg = len(arguments)
     print('{} arguments.'.format(num_arg))
     if num_arg == 0:
-        print(".")
+        print("")
     else:
         for num_arg, arg in enumerate(arguments, start=1):
             print("{}: {}".format(num_arg, arg))
