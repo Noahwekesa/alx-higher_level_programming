9b102def87de1eaeaca5f3aa663b1ae687623ce8
noahwksa <noahwekesa0gmail.com>
2023-08-11 15:51:38 +0300

    emotionally scared

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     from calculator_1 import *
     a = 10 
     b = 5
-    print('{a} + {a} = {}'.format(add(a, b)))
-    print('{a} - {b} = {}'.format(sub(a, b)))
-    print('{a} * {b} = {}'.format(mul(a, b)))
-    print('{a} / {b} = {}'.format(div(a, b)))
+    print('{} + {} = {}'.format(a, b, add(a, b)))
+    print('{} - {} = {}'.format(a, b, sub(a, b)))
+    print('{} * {} = {}'.format(a, b, mul(a, b)))
+    print('{} / {} = {}'.format(a, b, div(a, b)))
 
