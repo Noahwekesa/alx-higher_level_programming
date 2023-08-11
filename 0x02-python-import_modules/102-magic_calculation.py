83ae7b6d22d882704a43f4f933ae41326ae1f72a
noahwksa <noahwekesa0gmail.com>
2023-08-11 20:55:50 +0300

    cal

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation_102(a, b):
+    from calculator_1 import add, sub
+    if a < b:
+        c = add(a,b)
+        for numbers in range(4, 6):
+            c = add(c, numbers)
+            return (c)
+        else:
+            return (sub(a, b))
+        
