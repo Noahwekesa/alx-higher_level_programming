cb147d33bc16a4df216eea07ebccc89c6f827c47
noahwksa <noahwekesa0gmail.com>
2023-08-16 00:28:15 +0300

    tribulations

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 def max_integer(my_list=[]):
     if len(my_list) == 0:
-        return 0
+        return None
     max_num = my_list[0]
     for num in my_list:
         if num > max_num:
