eb3e9c6b705b19a9e7a3bd85ef5b3c3be959b1a2
noahwksa <noahwekesa0gmail.com>
2023-08-17 15:05:30 +0300

    programming

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    NewMatrix = [[num**2 for num in NewElement] for NewElement in matrix]
-    return NewMatrix
+    return [[num**2 for num in NewElement] for NewElement in matrix]
