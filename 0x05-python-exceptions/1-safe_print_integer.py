3216b79aa89e43bb2c257bffdb222c6f6e46559f
noahwksa <noahwekesa0gmail.com>
2023-08-29 01:52:59 +0300

    free

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,5 @@ def safe_print_integer(value):
     try:
         print("{}".format())
         return True
-    except (ValueError, TypeError):
+    except (ValueError, TypeError, IndexError):
         return False
