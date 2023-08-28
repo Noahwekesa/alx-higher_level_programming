72be6bfe163c0f9167b09ac94b8105da83b1f8fe
noahwksa <noahwekesa0gmail.com>
2023-08-29 01:46:00 +0300

    rasta

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[items]), end=" ")
             list += 1
-        except IndexError:
+        except ValueError:
             break
     print()
     return list
