4d097d9b7b7db315ff275b2151f13b7ecee4a109
noahwksa <noahwekesa0gmail.com>
2023-08-29 01:56:04 +0300

    she was the best

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     safe_list = 0
-    for items in range(x):
+    for elm in range(x):
         try:
-            print("{}".format(my_list[items]), end=" ")
+            print("{}".format(my_list[elm]), end=" ")
             safe_list += 1
-        except:
+        except ValueError:
             break
     print()
     return safe_list
