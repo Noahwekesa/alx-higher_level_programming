ac659a4f713d2ea9ebd26ceff08b3084a65e9a86
noahwksa <noahwekesa0gmail.com>
2023-08-29 22:24:08 +0300

    groupies

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
@@ -7,4 +8,3 @@ def safe_print_integer_err(value):
     except (TypeError, ValueError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
-    
