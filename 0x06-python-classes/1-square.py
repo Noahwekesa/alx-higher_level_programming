473f239c5b87403c6a845fd08a047a7fa964b349
noahwksa <noahwekesa0gmail.com>
2023-08-29 23:56:04 +0300

    task 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a class Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """class name Square
+
+    Attributes: size(private)
+    """
+    def __init__(self, size):
+        """
+        initialization of the private attribute
+        """
+        self.__size = size
+        
