268dbfeb9cf0feb425003c5187817776f4bf32d3
noahwksa <noahwekesa0gmail.com>
2023-08-31 00:41:55 +0300

    dont

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,22 +8,24 @@ class Square:
     """creates a square"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a square with a given size and position."""
+        """Initialize a square with a given:
+         size & position."""
         self.size = size
         self.position = position
 
     def __str__(self):
-        """Return a string representation of the square."""
-        return self.pos_print()
+        """Return:
+        string."""
+        return self.my_print()
 
     @property
     def size(self):
-        """Get the size of the square."""
+        """the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size of the square."""
+        """Set/initiates the size of the square."""
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
@@ -32,7 +34,7 @@ class Square:
 
     @property
     def position(self):
-        """Get the position of the square."""
+        """Getsthe position of the square."""
         return self.__position
 
     @position.setter
@@ -48,7 +50,7 @@ class Square:
         """Calculate the area of the square."""
         return self.__size * self.__size
 
-    def pos_print(self):
+    def my_print(self):
         """Generate the string representation of the square with position."""
         elm = ""
         if self.size == 0:
@@ -65,4 +67,4 @@ class Square:
 
     def my_print(self):
         """Print the string representation of the square with position."""
-        print(self.pos_print(), end='')
+        print(self.my_print(), end='')
