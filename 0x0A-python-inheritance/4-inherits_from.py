bd4118a75a2f3f08777bb5e0b38fe5525ccb6213
noahwksa <noahwekesa0gmail.com>
2023-09-11 21:06:28 +0300

    d

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,4 +6,6 @@ function to check inheritance
 
 def inherits_from(obj, a_class):
     """function to that implements logic"""
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+         return issubclass(type(obj), a_class)
+    return False
