15a58814f095180f6f3bcc745df068e208c1689f
noahwksa <noahwekesa0gmail.com>
2023-09-11 22:21:02 +0300

    kalonje

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,14 +5,13 @@ a class function that inherits from BaseGeometry(7-base_geometry.py)
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle:
+class Rectangle(BaseGeometry):
     """
     a class rectangle
     """
 
     def __init__(self, width, height):
-        """
-        function instantiation with w and h"""
+        """function instantiation with w and h"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
