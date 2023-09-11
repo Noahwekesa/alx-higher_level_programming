1245a8580cf8cd5ac87d4555a573f29d317e0ced
noahwksa <noahwekesa0gmail.com>
2023-09-11 20:59:09 +0300

    hendric

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function to check inheritance
+"""
+
+
+def inherits_from(obj, a_class):
+    """function to that implements logic"""
+    return issubclass(type(obj), a_class)
