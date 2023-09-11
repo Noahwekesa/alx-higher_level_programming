21f6acea9a21965f0ba4918c83a5c941ca62cb75
noahwksa <noahwekesa0gmail.com>
2023-09-11 20:23:54 +0300

    colossal

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+function that returns True of the obj is exacly an instance of the,
+specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
