7c745d1e827fc1a9d440b0cb9932f8931b971591
noahwksa <noahwekesa0gmail.com>
2023-09-11 21:17:30 +0300

    ceasar

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class based on 5-base_geometry"""
+
+
+class BaseGeometry:
+    """class called BaseGeomerty"""
+
+    def area(self):
+        """function implemted as requested"""
+        raise Exception("area() is not implemented")
