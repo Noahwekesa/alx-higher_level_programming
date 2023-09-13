2bf96b2434fed6cafe7d1a7ffbcc833acdb30fb4
noahwksa <noahwekesa0gmail.com>
2023-09-13 14:01:26 +0300

    drake

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,6 +5,7 @@ function to append text to a file aftr a specific string
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """ function implementation"""
     with open(filename, "r+") as file:
         lines = file.readlines()
         file.seek(0)
