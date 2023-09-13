aa6c5e69be7cb0fad0623b9deb852d96927ef2b8
noahwksa <noahwekesa0gmail.com>
2023-09-13 13:20:15 +0300

    ddd

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,19 +6,19 @@ def pascal_triangle(n):
     """function method question"""
     if n <= 0:
         return []
-    
+
     triangle = []
-    
+
     for rowIndex in range(n):
         row = []
-        
+
         for i in range(rowIndex + 1):
             if i == 0 or i == rowIndex:
                 row.append(1)
             else:
                 element = row[i-1] * (rowIndex - i + 1) // i
                 row.append(element)
-        
+
         triangle.append(row)
-    
+
     return triangle
