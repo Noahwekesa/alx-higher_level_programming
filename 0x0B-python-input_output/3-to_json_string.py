df105614cb804afcc55c33f7ca6ca742b10d5b1c
noahwksa <noahwekesa0gmail.com>
2023-09-13 11:17:47 +0300

    json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Function to Return JSON Representation of an Object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """function implementation"""
+    return json.dumps(my_obj)
