f8b46a07a32403c47db5225dfd2d022d67e0ba42
noahwksa <noahwekesa0gmail.com>
2023-09-13 10:30:47 +0300

    write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-function module that writes a string to a text file (UTF8) and returns the number of characters written.
+function module that writes a string to a text file (UTF8)
+and returns the number of characters written.
 """
 
 
-def write_file(filename="",text=""):
+def write_file(filename="", text=""):
     """function implementation"""
     with open(filename, 'w', encoding="UTF8") as file:
         file.write(text)
