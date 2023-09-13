55a13f62f59bf7375af4427c457d61c4eb57debd
noahwksa <noahwekesa0gmail.com>
2023-09-13 11:47:25 +0300

    load sav

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Script to add arguments to a Python List and Save to a File"""
 import sys
-
-
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 arguments = []
 
