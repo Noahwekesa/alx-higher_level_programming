dfa19cb08111ac6368d1b6cba0fb0e3330c25bcd
noahwksa <noahwekesa0gmail.com>
2023-09-13 10:16:49 +0300

    first q

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+ function that reads a text file (UTF8) and prints it to stdout.
+"""
+
+
+def read_file(filename=""):
+    """implement function"""
+    with open(filename, 'r', encoding='utf8')as file:
+        print(file.read())
