80291ed95f8e610e49877d5e6aec1b1108d736f9
noahwksa <noahwekesa0gmail.com>
2023-10-17 23:37:52 +0300

    fnf

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+--Script to Convert hbtn_0c_0 Database to UTF8
+SHOW CREATE DATABASE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SHOW CREATE TABLE hbtn_0c_0.first_table;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SHOW FULL COLUMNS FROM hbtn_0c_0.first_table LIKE 'name';
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+--Script to List Records of the second_table
+SELECT score, name
+FROM hbtn_0c_0.second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
