167ffa8f96cabba248e340f0f45ed6081590319e
noahwksa <noahwekesa0gmail.com>
2023-10-19 22:47:21 +0300

    djf

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
--- Script to List Records with Score >= 10 in second_table
+--  Script to List Records with Score >= 10 in second_table.
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
